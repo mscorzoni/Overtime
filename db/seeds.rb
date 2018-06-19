a209d0775b6239530bccca8fe7885fa95865625f
Marcos Scorzoni <mlessa@gmail.com>
2018-06-19 19:41:44 +0100

    show approval status admin dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 @user = User.create(email: "test@test.com", password: "111111", password_confirmation: "111111", first_name: "Jon", last_name: "Snow")
 puts 'user just created'
-puts 'We are about to create 100 posts'
+
+AdminUser.create(email: "admin@admin.com", password: "111111", password_confirmation: "111111", first_name: "Admin", last_name: "Admin")
+
+puts 'admin just created'
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id) 
 end
