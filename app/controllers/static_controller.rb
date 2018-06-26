51c85e18c7ed22ffd4ec43ddcc038ad3cf48e87f
Marcos Scorzoni <mlessa@gmail.com>
2018-06-26 19:11:51 +0100

    live data for admin review page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   skip_before_action :authenticate_user!, only: [:homepage]
   def homepage
+    @pending_approvals = Post.where(status: 'submitted')
   end
 end
