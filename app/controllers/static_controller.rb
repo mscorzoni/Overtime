2127f2f468047396a14b41ea5a77466de477bfa4
Marcos Scorzoni <mlessa@gmail.com>
2018-06-27 12:04:16 +0100

    add new views elements for pending view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,5 +2,6 @@ class StaticController < ApplicationController
   skip_before_action :authenticate_user!, only: [:homepage]
   def homepage
     @pending_approvals = Post.where(status: 'submitted')
+    @recent_audit_items = AuditLog.last(10)
   end
 end
