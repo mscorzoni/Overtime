b9b9173eaa358c0f24c7e1fb319c5b07714c1620
Marcos Scorzoni <mlessa@gmail.com>
2018-06-29 12:47:59 +0100

    production.rb setup for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.server_static_assets = true
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
