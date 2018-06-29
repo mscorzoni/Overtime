8cf2da78b7006153560e71f77e3ed66f71a869b0
Marcos Scorzoni <mlessa@gmail.com>
2018-06-29 14:51:06 +0100

    add employee model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-@user = User.create(email: "test@test.com", 
+@employee = Employee.create(email: "test@test.com", 
                     password: "111111", 
                     password_confirmation: "111111", 
                     first_name: "Jon", 
                     last_name: "Snow",
                     phone: '1494372947')
-puts 'user just created'
+puts 'employee just created'
 
 AdminUser.create(email: "admin@admin.com",
                  password: "111111",
@@ -16,15 +16,15 @@ AdminUser.create(email: "admin@admin.com",
 puts 'admin just created'
 
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 
 puts '3 Audit Logs have been created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5) 
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @employee.id, overtime_request: 2.5) 
 end
 puts '100 Posts have been created'
 
