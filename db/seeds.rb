2be3c43e6735cdc1dc869e493e04e0080e9d4e1a
Marcos Scorzoni <mlessa@gmail.com>
2018-07-03 13:24:59 +0100

    update daily hours column through app

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.
 puts '3 Audit Logs have been created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @employee.id, overtime_request: 2.5) 
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @employee.id, daily_hours: 2.5) 
 end
 puts '100 Posts have been created'
 
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,21 +2,21 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some rationale"
-    overtime_request 1.2
+    daily_hours 1.2
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
-    overtime_request 0.3
+    daily_hours 0.3
     user
   end
 
   factory :post_from_non_authorized_user, class: "Post" do
     date Date.yesterday
     rationale "Non Auhtorized Post"
-    overtime_request 1.2
+    daily_hours 1.2
     non_authorized_user
   end
 end
